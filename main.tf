5b1bef4ff42946aa0b05ab91e94329d966737d32
modridi <mohamed-amine.dridi@camptocamp.com>
2023-09-18 15:55:21 +0200

    feat: render module

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "null_resource" "dependencies" {
 
 resource "argocd_project" "this" {
   metadata {
-    name      = "<CHART_NAME>"
+    name      = "vault"
     namespace = var.argocd_namespace
     annotations = {
       "devops-stack.io/argocd_namespace" = var.argocd_namespace
@@ -12,8 +12,8 @@ resource "argocd_project" "this" {
   }
 
   spec {
-    description  = "<CHART_NAME> application project"
-    source_repos = ["https://github.com/camptocamp/devops-stack-module-<CHART_NAME>.git"]
+    description  = "vault application project"
+    source_repos = ["https://github.com/camptocamp/devops-stack-module-vault.git"]
 
     destination {
       name      = "in-cluster"
@@ -37,7 +37,7 @@ data "utils_deep_merge_yaml" "values" {
 
 resource "argocd_application" "this" {
   metadata {
-    name      = "<CHART_NAME>"
+    name      = "vault"
     namespace = var.argocd_namespace
   }
 
@@ -52,11 +52,15 @@ resource "argocd_application" "this" {
     project = argocd_project.this.metadata.0.name
 
     source {
-      repo_url        = "https://github.com/camptocamp/devops-stack-module-<CHART_NAME>.git"
-      path            = "charts/<CHART_NAME>"
+      repo_url        = "https://github.com/camptocamp/devops-stack-module-vault.git"
+      path            = "charts/vault"
       target_revision = var.target_revision
       helm {
         values = data.utils_deep_merge_yaml.values.output
+        parameter {
+          name  = "vault.server.dev.devRootToken"
+          value = sensitive(var.dev_root_token)
+        }
       }
     }
 
